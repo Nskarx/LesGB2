ca4a7c0e97fd85d92073835c1d1382a72a46fa06
Evgeniy <Lymilight@gmail.com>
2023-03-06 20:07:26 +0700

    add string

---
+++ Exam_02/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Console.ReadLine();с помощью нее происходит считывание данных
+// string (строка) -это тип данных и место где хвариться информация
+//username-имя переменной
+// = (Оператор) означает- возьми то что справа и положи в то что слева
+// Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("привет,");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("привет,");
+Console.Write(username);
